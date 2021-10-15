f0e0aef2cd08c1c9c6e857f3e4d778288e8480a3
Padmashree Jha <codewithpom@gmail.com>
2021-10-15 12:13:14 +0000

    Initialized the first project

---
+++ axolotl/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h1>Axolotl</h1>
+    <img class="img-fluid" v-bind:src="url" />
+  </div>
+
+</template>
+
+<script>
+
+
+const url = "https://media.discordapp.net/attachments/808700312881070120/848290622937759754/CU_TE.jpg"
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      url: url,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
